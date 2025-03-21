bba542c869d39c9b760068f8df2dbc3946f91ecf
Bruno Magalhães Prates Pereira <brunomagalhaespp@hotmail.com>
2025-03-21 14:21:48 -0300

    Add a basic test

---
+++ cypress/e2e/3-integration/basic.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+
+
+describe('Cypress Basics', ()=>{
+
+    it('Should visit a page and assert title', ()=>{
+
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+
+         
+    })
+})
